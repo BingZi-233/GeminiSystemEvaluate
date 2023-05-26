bc250d3267064f42ecd348b12aa1ef0016b508b7
冰子 <lhby233@outlook.com>
2023-05-26 18:47:16 +0800

    Initial commit

---
+++ src/main/kotlin/online/bingzi/gemini/systemEvaluate/GeminiSystemEvaluate.kt
@@ -0,0 +1,11 @@
+package online.bingzi.gemini.systemEvaluate
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object GeminiSystemEvaluate : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
